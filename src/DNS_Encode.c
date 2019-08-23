98c5d1f98c1cf357a5d165302fdb9d712284b08b
Tzu-yi CHIU <tzu-yi.chiu@polytechnique.edu>
2019-08-24 00:52:49 +0200

    --- Added bytes_to_DNS method ---

--- src/DNS_Encode.c
+++ src/DNS_Encode.c
@@ -95,102 +95,111 @@ int qname_to_bytes(unsigned char* msg, unsigned char* qname)
 int DNS_to_bytes(unsigned char* bytes, DNS_PACKET dns_packet)
 {
     // header
-    bytes[0]  		 =	(dns_packet.header->id       >>  8) & 0xFF;
-    bytes[1]  		 =	(dns_packet.header->id       >>  0) & 0xFF;
-
-    bytes[2]  		 = ((dns_packet.header->rd 	 	 <<  0) & 0x01) |
-               	       ((dns_packet.header->tc       <<  1) & 0x02) |
-               	       ((dns_packet.header->aa       <<  2) & 0x04) | 
-               		   ((dns_packet.header->opcode   <<  3) & 0x78) |
-               		   ((dns_packet.header->qr       <<  7) & 0x80);
-
-    bytes[3]  		 = ((dns_packet.header->rcode    <<  0) & 0x0F) |
-               		   ((dns_packet.header->z        <<  4) & 0x70) |
-               		   ((dns_packet.header->ra       <<  7) & 0x80);
-
-    bytes[4]  		 =	(dns_packet.header->qdcount  >>  8) & 0xFF;
-    bytes[5]  		 =	(dns_packet.header->qdcount  >>  0) & 0xFF;
-
-    bytes[6]  		 =	(dns_packet.header->ancount  >>  8) & 0xFF;
-    bytes[7]  		 =	(dns_packet.header->ancount  >>  0) & 0xFF;
-
-    bytes[8]  		 =	(dns_packet.header->nscount  >>  8) & 0xFF;
-    bytes[9]  		 =	(dns_packet.header->nscount  >>  0) & 0xFF;
-
-    bytes[10] 		 =	(dns_packet.header->arcount  >>  8) & 0xFF;
-    bytes[11] 		 =	(dns_packet.header->arcount  >>  0) & 0xFF;
+    bytes[0]  		 = ((dns_packet.header->id       >>  8) & 0xFF);
+    bytes[1]  		 = ((dns_packet.header->id       >>  0) & 0xFF);
+    bytes[2]  		 = ((dns_packet.header->qr       <<  7) & 0x80)|
+    				   ((dns_packet.header->opcode   <<  3) & 0x78)|
+    				   ((dns_packet.header->aa       <<  2) & 0x04)|
+    				   ((dns_packet.header->tc       <<  1) & 0x02)|
+    				   ((dns_packet.header->rd 	 	 <<  0) & 0x01);
+    bytes[3]  		 = ((dns_packet.header->ra       <<  7) & 0x80)|
+    				   ((dns_packet.header->z        <<  4) & 0x70)|
+    				   ((dns_packet.header->rcode    <<  0) & 0x0F);
+    bytes[4]  		 = ((dns_packet.header->qdcount  >>  8) & 0xFF);
+    bytes[5]  		 = ((dns_packet.header->qdcount  >>  0) & 0xFF);
+    bytes[6]  		 = ((dns_packet.header->ancount  >>  8) & 0xFF);
+    bytes[7]  		 = ((dns_packet.header->ancount  >>  0) & 0xFF);
+    bytes[8]  		 = ((dns_packet.header->nscount  >>  8) & 0xFF);
+    bytes[9]  		 = ((dns_packet.header->nscount  >>  0) & 0xFF);
+    bytes[10] 		 = ((dns_packet.header->arcount  >>  8) & 0xFF);
+    bytes[11] 		 = ((dns_packet.header->arcount  >>  0) & 0xFF);
     
     // question
     int offset = qname_to_bytes(bytes[12], dns_packet.question->qname)
     
-    bytes[11+offset] = 	(dns_packet.question->qtype  >>  8) & 0xFF;
-    bytes[12+offset] = 	(dns_packet.question->qtype  >>  0) & 0xFF;
-
-    bytes[13+offset] = 	(dns_packet.question->qclass >>  8) & 0xFF;
-    bytes[14+offset] = 	(dns_packet.question->qclass >>  0) & 0xFF;
+    bytes[11+offset] = ((dns_packet.question->qtype  >>  8) & 0xFF);
+    bytes[12+offset] = ((dns_packet.question->qtype  >>  0) & 0xFF);
+    bytes[13+offset] = ((dns_packet.question->qclass >>  8) & 0xFF);
+    bytes[14+offset] = ((dns_packet.question->qclass >>  0) & 0xFF);
 
     // answer
-
     offset += qname_to_bytes(bytes[12], dns_packet.answer->name)
-    
-    bytes[14+offset] = 	(dns_packet.answer->type 	 >>  8) & 0xFF;
-    bytes[15+offset] = 	(dns_packet.answer->type 	 >>  0) & 0xFF;
 
-    bytes[16+offset] = 	(dns_packet.answer->rclass 	 >>  8) & 0xFF;
-    bytes[17+offset] = 	(dns_packet.answer->rclass 	 >>  0) & 0xFF;
-
-    bytes[18+offset] = 	(dns_packet.answer->ttl 	 >> 24) & 0xFF;
-    bytes[19+offset] = 	(dns_packet.answer->ttl		 >> 16) & 0xFF;
-    bytes[20+offset] = 	(dns_packet.answer->ttl 	 >>  8) & 0xFF;
-    bytes[21+offset] = 	(dns_packet.answer->ttl 	 >>  0) & 0xFF;
-
-    bytes[22+offset] = 	(dns_packet.answer->rdlength >>  8) & 0xFF;
-    bytes[23+offset] = 	(dns_packet.answer->rdlength >>  0) & 0xFF;
-
-    bytes[24+offset] =  dns_packet.answer->rdata;
+    bytes[14+offset] = ((dns_packet.answer->type 	 >>  8) & 0xFF);
+    bytes[15+offset] = ((dns_packet.answer->type 	 >>  0) & 0xFF);
+    bytes[16+offset] = ((dns_packet.answer->rclass 	 >>  8) & 0xFF);
+    bytes[17+offset] = ((dns_packet.answer->rclass 	 >>  0) & 0xFF);
+    bytes[18+offset] = ((dns_packet.answer->ttl 	 >> 24) & 0xFF);
+    bytes[19+offset] = ((dns_packet.answer->ttl		 >> 16) & 0xFF);
+    bytes[20+offset] = ((dns_packet.answer->ttl 	 >>  8) & 0xFF);
+    bytes[21+offset] = ((dns_packet.answer->ttl 	 >>  0) & 0xFF);
+    bytes[22+offset] = ((dns_packet.answer->rdlength >>  8) & 0xFF);
+    bytes[23+offset] = ((dns_packet.answer->rdlength >>  0) & 0xFF);
+    bytes[24+offset] = ((dns_packet.answer->rdata    >>  0) & 0xFF);
 
     return 24 + offset + dns_packet.answer->rdlength;
 }
 
 
-void bytes_to_DNS(DNS_PACKET dns_packet, unsigned char* bytes, int len_bytes)
+void bytes_to_DNS(DNS_PACKET dns_packet, unsigned char* bytes)
 {
-	printf("header->id:                 %d %d\n",   dns_packet.header->id / 256, 
-                                                    dns_packet.header->id % 256);
-    printf("header->qr:                 %d\n",      dns_packet.header->qr);
-    printf("header->opcode:             %d\n",      dns_packet.header->opcode);
-    printf("header->aa:                 %d\n",      dns_packet.header->aa);
-    printf("header->tc:                 %d\n",      dns_packet.header->tc);
-    printf("header->rd:                 %d\n",      dns_packet.header->rd);
-    printf("header->ra:                 %d\n",      dns_packet.header->ra);
-    printf("header->z:                  %d\n",      dns_packet.header->z);
-    printf("header->rcode:              %d\n",      dns_packet.header->rcode);    
-    printf("header->qdcount:            %d %d\n",   dns_packet.header->qdcount / 256, 
-                                                    dns_packet.header->qdcount % 256);
-    printf("header->ancount:            %d %d\n",   dns_packet.header->ancount / 256, 
-                                                    dns_packet.header->ancount % 256);
-    printf("header->nscount:            %d %d\n",   dns_packet.header->nscount / 256, 
-                                                    dns_packet.header->nscount % 256);
-    printf("header->arcount:            %d %d\n",   dns_packet.header->arcount / 256, 
-                                                    dns_packet.header->arcount % 256);
-    printf("question->qname:            ")
-    for (int i=0; i<sizeof(dns_packet.question->qname); i++)
-        printf("%d ",                               dns_packet.question->qname[i]);
-    printf("\n");
+    int offset = 0;
 
-    printf("question->qtype:            %d\n",      dns_packet.question->qtype);
-    printf("question->qclass:           %d\n",      dns_packet.question->qclass);
-    printf("answer->name:               %d %d\n",   dns_packet.answer->name / 256,
-                                                    dns_packet.answer->name % 256);
-    printf("answer->resource->type:     %d\n",      dns_packet.answer->resource->type);
-    printf("answer->resource->rclass:   %d\n",      dns_packet.answer->resource->rclass);    
-    printf("answer->resource->ttl:      %d\n",      dns_packet.answer->resource->ttl);
-    printf("answer->resource->rdlength: %d\n",      dns_packet.answer->resource->rdlength);
+    // header
+    dns_packet.header->id 				  =	((bytes[ 0+offset] << 8) & 0xFF00     )| 
+    										((bytes[ 1+offset] << 0) & 0xFF  	  );
+    dns_packet.header->qr 				  =	((bytes[ 2+offset] >> 7) & 0x01		  );
+    dns_packet.header->opcode 			  =	((bytes[ 2+offset] >> 3) & 0x80 	  );
+    dns_packet.header->aa 				  =	((bytes[ 2+offset] >> 2) & 0x01		  );
+    dns_packet.header->tc 				  =	((bytes[ 2+offset] >> 1) & 0x01		  );
+    dns_packet.header->rd 				  = ((bytes[ 2+offset] >> 0) & 0x01		  );
+    dns_packet.header->ra 				  =	((bytes[ 3+offset] >> 7) & 0x01 	  );
+    dns_packet.header->z				  =	((bytes[ 3+offset] >> 4) & 0x08 	  );
+    dns_packet.header->rcode			  =	((bytes[ 3+offset] >> 0) & 0x0F 	  );
+    dns_packet.header->qdcount			  =	((bytes[ 4+offset] << 8) & 0xFF00 	  )| 
+    										((bytes[ 5+offset] << 0) & 0xFF 	  );
+    dns_packet.header->qdcount			  = ((bytes[ 6+offset] << 8) & 0xFF00 	  )| 
+    										((bytes[ 7+offset] << 0) & 0xFF 	  );
+    dns_packet.header->ancount			  = ((bytes[ 8+offset] << 8) & 0xFF00 	  )| 
+    										((bytes[ 9+offset] << 0) & 0xFF 	  );
+    dns_packet.header->arcount			  = ((bytes[10+offset] << 8) & 0xFF00 	  )| 
+    										((bytes[11+offset] << 0) & 0xFF  	  );
     
-    printf("answer->rdata:              ")
+    // question
+    while (bytes[12+offset] != '\0')
+    {
+    	dns_packet.question->qname[offset] = bytes[12+offset];
+    	offset++;
+    }
+    dns_packet.question->qname[offset] = '\0';
+
+    dns_packet.question->qtype 			  =	((bytes[13+offset] << 8) & 0xFF00     )|
+    										((bytes[14+offset] << 0) & 0xFF  	  );
+    dns_packet.question->qclass           =	((bytes[15+offset] << 8) & 0xFF00     )|
+    										((bytes[16+offset] << 0) & 0xFF 	  );
+
+    // answer
+    int offset0 = offset;
+    while (bytes[17+offset] != '\0')
+    {
+    	dns_packet.answer->name[offset-offset0] = bytes[17+offset];
+    	offset++;
+    }
+    dns_packet.answer->name[offset-offset0] = '\0';
+     
+    dns_packet.answer->resource->type     =	((bytes[18+offset] <<  8) & 0xFF00    )| 
+    									  	((bytes[19+offset] <<  0) & 0xFF  	  );
+    dns_packet.answer->resource->rclass   =	((bytes[20+offset] <<  8) & 0xFF00 	  )|
+    									  	((bytes[21+offset] <<  0) & 0xFF      );
+    dns_packet.answer->resource->ttl	  =	((bytes[22+offset] << 24) & 0xFF000000)| 
+    								   	  	((bytes[23+offset] << 16) & 0xFF0000  )|
+    								   	  	((bytes[24+offset] <<  8) & 0xFF00    )|
+    								   	  	((bytes[25+offset] <<  0) & 0xFF      );
+    dns_packet.answer->resource->rdlength = ((bytes[26+offset] <<  8) & 0xFF00    )| 
+    										((bytes[27+offset] <<  0) & 0xFF      );
+    
     for (int i=0; i<dns_packet.answer->resource->rdlength; i++)
-        printf("%d ",                               dns_packet.answer->rdata[i]);
-    printf("\n");
+    	dns_packet.answer->rdata[i] = bytes[28+offset+i];
 }
 
 
